cdef370f539b946b44a63a1bbbf74aec7bd9d291
RashanWeerasinghe <malithrashan03@gmail.com>
2023-12-21 17:33:44 +0530

    init

---
+++ ChannelHealthPro/src/main/java/com/ChannelHealthPro/ChannelHealthPro/ChannelHealthProApplication.java
@@ -0,0 +1,13 @@
+package com.ChannelHealthPro.ChannelHealthPro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChannelHealthProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChannelHealthProApplication.class, args);
+	}
+
+}
---
+++ ChannelHealthPro/src/test/java/com/ChannelHealthPro/ChannelHealthPro/ChannelHealthProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ChannelHealthPro.ChannelHealthPro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChannelHealthProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
